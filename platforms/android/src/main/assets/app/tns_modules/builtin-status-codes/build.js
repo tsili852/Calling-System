0a0e19f14e7fe062e5209c0a1b0a7200deaa5210
tsili852 <nick.tsilivis@gmail.com>
2017-10-12 08:24:50 +0200

    12/10/17

--- platforms/android/src/main/assets/app/tns_modules/builtin-status-codes/build.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-
-var fs = require('nativescript-node/fs')
-var statusCodes = require('./')
-
-var code = 'module.exports = ' + JSON.stringify(statusCodes, null, 2) + '\n'
-
-fs.writeFileSync('browser.js', code)
